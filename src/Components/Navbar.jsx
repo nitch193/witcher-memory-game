c0f6e6e8de04c47a1e081a72d68dcc2923b6e935
nitch193 <niteshchaurasiya193@gmail.com>
2021-04-13 22:56:33 +0530

    new project for learning hooks in react

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Witcher Memory Game</h1>
+      <div>
+        <h4>Score : {0}</h4>
+        <h4>Highest- Score : {0}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
